943919d94a7e882a32b9c9bced392cecfdcdfea2
Iydon <11711217@mail.sustc.edu.cn>
2018-07-03 08:38:32 +0800

    MAT2PY

--- MAT2PY/mat2py.py
+++ MAT2PY/mat2py.py
@@ -1,11 +1,11 @@
 from re import split, findall, sub
 import numpy as np
 
-	
+    
 def mat2lst(content):
-	# '[1,2;3,4]' -> [[1,2],[3,4]]
-	# str         -> list
-	return [[[float(row) for row in split('[, ]+',row)] for row in split(';',lst)] for lst in findall('(?<=\[)[\s\S]+?(?=\])', sub('((?<=[^\d\.]) +)|( +(?=[^\d\-+]))', '', sub(' +',' ',content)))]
+    # '[1,2;3,4]' -> [[1,2],[3,4]]
+    # str         -> list
+    return [[[float(row) for row in split('[, ]+',row)] for row in split(';',lst)] for lst in findall('(?<=\[)[\s\S]+?(?=\])', sub('((?<=[^\d\.]) +)|( +(?=[^\d\-+]))', '', sub(' +',' ',content)))]
 
 lsts = matstr2pylst('[1,-2; +3. 4] [1. -2 ; 3. +4], [1, 2 ;3   +4] ! [1;2. ; 3 ;-4]. [-1 , 2 ;3.,4]')
 print(lsts)
@@ -13,23 +13,23 @@ print(lsts)
 
 
 def op2mat(s):
-	# 'T(x1,x2)=(x2,x1)' -> [[0,1],[1,0]]
-	# str                -> list
-	s     = s.replace(' ','')
-	flg   = re.findall('^[a-zA-Z0-9]+(?=\()', s)[0]
-	parts = re.findall('(?<=\()[\S]+?(?=\))', s)
+    # 'T(x1,x2)=(x2,x1)' -> [[0,1],[1,0]]
+    # str                -> list
+    s     = s.replace(' ','')
+    flg   = re.findall('^[a-zA-Z0-9]+(?=\()', s)[0]
+    parts = re.findall('(?<=\()[\S]+?(?=\))', s)
 
-	V = re.split(',', parts[0])
-	W = re.split(',', parts[1])
-	M = [[0]*len(W) for i in range(0, len(V))]
+    V = re.split(',', parts[0])
+    W = re.split(',', parts[1])
+    M = [[0]*len(W) for i in range(0, len(V))]
 
-	for i in range(0, len(V)):
-		for j in range(0, len(W)):
-			if V[i] in W[j]:
-				tmp = re.findall('[\+\-]?[\d\.e]+(?=\*%s)'%V[i], W[j])
-				M[i][j] = float(tmp[0]) if tmp else float(1)
-	
-	return np.matrix(M)
+    for i in range(0, len(V)):
+        for j in range(0, len(W)):
+            if V[i] in W[j]:
+                tmp = re.findall('[\+\-]?[\d\.e]+(?=\*%s)'%V[i], W[j])
+                M[i][j] = float(tmp[0]) if tmp else float(1)
+    
+    return np.matrix(M)
 
 res = 'T(a1,a2,a3,a4,a5,a6,a7)=(a2,a3,a4,a5,a6,a7,a1)'
 print(res)
